1edfbf929759aaeb2c9e6c0a888244ed51f364a5
Suronix <anas.lemkaddem@gmail.com>
2023-02-16 23:19:29 +0100

    Refactor and added Bootstrap

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className="App">
+        <div className="container py-4 px-3 mx-auto">
+            <h1>Hello, Bootstrap and Vite!</h1>
+            <button className="btn btn-primary">Primary button</button>
+        </div>
+    </div>
+  )
+}
+
+export default App
