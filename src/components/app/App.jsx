39c038ca0ac605331a44208618310cc0fc2bcc85
Suronix <anas.lemkaddem@gmail.com>
2023-03-19 01:33:11 +0100

    Fixed Storybbook esllint now working

---
+++ src/components/app/App.jsx
@@ -0,0 +1,14 @@
+import Button from '../buttons/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container py-4 px-3 mx-auto">
+        <h1>Hello, Bootstrap and Vite!</h1>
+        <Button className="btn btn-primary">Primary</Button>
+      </div>
+    </div>
+  )
+}
+
+export default App
